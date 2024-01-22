a06fef1e434f6c09acee6b3184410dc2bb08a0fb
Carl Ising <carle3453@hotmail.com>
2024-01-22 22:49:59 +0100

    Added spawntype to spawns

---
+++ Enums/SpawnType.cs
@@ -0,0 +1,9 @@
+
+namespace CS2Executes.Enums
+{
+	public enum ESpawnType : int
+	{
+		SPAWNFLAG_LURKER = (1 << 0),
+		SPAWNFLAG_ENTRY = (1 << 1),
+	}
+}
--- Models/Spawn.cs
+++ Models/Spawn.cs
@@ -4,23 +4,27 @@ using CS2Executes.Enums;
 
 namespace CS2Executes.Models
 {
-	public class Grenades
-	{
-		
-	}
 	public class Spawn
 	{
 		public string Name { get; set; }
 		public CsTeam Team { get; set; }
 		public Vector Position { get; set; }
 		public QAngle Angle { get; set; }
+		public ESpawnType SpawnType { get; set; }
 
-		public Spawn(string name, CsTeam team, Vector position, QAngle angle)
+
+		public Spawn(string name, CsTeam team, Vector position, QAngle angle, ESpawnType spawnType)
 		{
 			Name = name;
 			Team = team;
 			Position = position;
 			Angle = angle;
+			SpawnType = spawnType;
+		}
+
+		public ESpawnType GetSpawnFlags()
+		{
+			return SpawnType;
 		}
 	}
 }
