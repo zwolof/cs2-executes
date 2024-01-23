40de8acc6c69f08a353b37ac22f2e41944d5794a
Carl Ising <carle3453@hotmail.com>
2024-01-23 02:40:40 +0100

    Modified spawn structure

--- Enums/Bombsite.cs
+++ Enums/Bombsite.cs
@@ -9,27 +9,4 @@ namespace CS2Executes.Enums
 
 		B = 1
 	}
-
-	public static class EBombsiteExtension
-	{
-		public static string ToString(this EBombsite bombsite) 
-		{
-			return bombsite switch
-			{
-				EBombsite.A => "A",
-				EBombsite.B => "B",
-				_ => "None",
-			};
-		}
-
-		public static EBombsite ToEnum(this string bombsite)
-		{
-			return bombsite switch
-			{
-				"A" => EBombsite.A,
-				"B" => EBombsite.B,
-				_ => EBombsite.A,
-			};
-		}
-	}
 }
--- Enums/SpawnType.cs
+++ Enums/SpawnType.cs
@@ -3,7 +3,7 @@ namespace CS2Executes.Enums
 {
 	public enum ESpawnType : int
 	{
-		SPAWNFLAG_LURKER = (1 << 0),
-		SPAWNFLAG_ENTRY = (1 << 1),
+		SPAWNTYPE_LURKER,
+		SPAWNTYPE_ENTRY
 	}
 }
